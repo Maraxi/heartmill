46a968048b6a329947095c95fd076a5a779dad37
Maraxi <Maraxi@users.noreply.github.com>
2024-07-16 11:32:53 +0200

    scrollwheel script

--- website files/scrollwheel.js
+++ website files/scrollwheel.js
@@ -2,7 +2,22 @@
   let scrollElement = document.querySelector("#Main");
 
   window.addEventListener("wheel", (event) => {
-    if (!event.shiftKey && event.deltaY != 0) {
+    /* do not scroll horizontal when focusing dustJacket with scrollbar */
+    let isJacket = (ele) => ele.classList.contains("dustJacketText");
+    if (isJacket(event.target) || isJacket(event.target.parentElement)) {
+      let jacket = isJacket(event.target)
+        ? event.target
+        : event.target.parentElement;
+      if (
+        event.deltaY > 0 &&
+        jacket.clientHeight + jacket.scrollTop < jacket.scrollHeight
+      ) {
+        return;
+      }
+    }
+
+    /* add vertical mousewheel to horizontal scroll */
+    if (!event.shiftKey && event.deltaY != 0 && !isJacket(event.target)) {
       if (Math.abs(event.deltaY) >= 100) {
         /* mousewheel */
         scrollElement.scrollBy({
