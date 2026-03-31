ce220006857479a5a51e7f798196ec9bddb7a3ba
Maraxi <Maraxi@users.noreply.github.com>
2026-03-31 22:08:18 +0200

    Add download script

---
+++ download.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# Download current state of website recursivly
+
+ wget --no-parent -r https://heartmill.com
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
+
+# Host locally
 
 python -m http.server -d "website files" &
